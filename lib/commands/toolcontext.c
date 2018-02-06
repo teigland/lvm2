7577c73a38cc097e6a567bc4488aa1db8deb5b05
David Teigland <teigland@redhat.com>
2018-02-06 15:18:11 -0600

    scan: use bcache for label scan and vg read
    New label_scan function populates bcache for each device
    on the system.
    
    The two read paths are updated to get data from bcache.
    
    The bcache is not yet used for writing.  bcache blocks
    for a device are invalidated when the device is written.

--- lib/commands/toolcontext.c
+++ lib/commands/toolcontext.c
@@ -2112,6 +2112,7 @@ int refresh_toolcontext(struct cmd_context *cmd)
 
 	activation_release();
 	lvmcache_destroy(cmd, 0, 0);
+	label_scan_destroy(cmd);
 	label_exit();
 	_destroy_segtypes(&cmd->segtypes);
 	_destroy_formats(cmd, &cmd->formats);
@@ -2231,6 +2232,7 @@ void destroy_toolcontext(struct cmd_context *cmd)
 	archive_exit(cmd);
 	backup_exit(cmd);
 	lvmcache_destroy(cmd, 0, 0);
+	label_scan_destroy(cmd);
 	label_exit();
 	_destroy_segtypes(&cmd->segtypes);
 	_destroy_formats(cmd, &cmd->formats);
