71187215d48e4518f2cd0dea6499493887da91f9
David Teigland <teigland@redhat.com>
2018-02-07 13:26:37 -0600

    scan: do scanning at the start of a command
    Move the location of scans to make it clearer and avoid
    unnecessary repeated scanning.  There should be one scan
    at the start of a command which is then used through the
    rest of command processing.
    
    Previously, the initial label scan was called as a side effect
    from various utility functions.  This would lead to it being called
    unnecessarily.  It is an expensive operation, and should only be
    called when necessary.  Also, this is a primary step in the
    function of the command, and as such it should be called prominently
    at the top level of command processing, not as a hidden side effect
    of a utility function.  lvm knows exactly where and when the
    label scan needs to be done.  Because of this, move the label scan
    calls from the internal functions to the top level of processing.
    
    Other specific instances of lvmcache_label_scan() are still called
    unnecessarily or unclearly by specific commands that do not use
    the common process_each functions.  These will be improved in
    future commits.
    
    During the processing phase, rescanning labels for devices in a VG
    needs to be done after the VG lock is acquired in case things have
    changed since the initial label scan.  This was being done by way
    of rescanning devices that had the INVALID flag set in lvmcache.
    This usually approximated the right set of devices, but it was not
    exact, and obfuscated the real requirement.  Correct this by using
    a new function that rescans the devices in the VG:
    lvmcache_label_rescan_vg().
    
    Apart from being inexact, the rescanning was extremely well hidden.
    _vg_read() would call ->create_instance(), _text_create_text_instance(),
    _create_vg_text_instance() which would call lvmcache_label_scan()
    which would call _scan_invalid() which repeats the label scan on
    devices flagged INVALID.  lvmcache_label_rescan_vg() is now called
    prominently by _vg_read() directly.

--- tools/toollib.c
+++ tools/toollib.c
@@ -2228,14 +2228,10 @@ int process_each_vg(struct cmd_context *cmd,
 	}
 
 	/*
-	 * First rescan for available devices, then force the next
-	 * label scan to be done.  get_vgnameids() will scan labels
-	 * (when not using lvmetad).
+	 * Scan all devices to populate lvmcache with initial
+	 * list of PVs and VGs.
 	 */
-	if (cmd->cname->flags & REQUIRES_FULL_LABEL_SCAN) {
-		dev_cache_full_scan(cmd->full_filter);
-		lvmcache_force_next_label_scan();
-	}
+	lvmcache_label_scan(cmd);
 
 	/*
 	 * A list of all VGs on the system is needed when:
@@ -3740,6 +3736,12 @@ int process_each_lv(struct cmd_context *cmd,
 	}
 
 	/*
+	 * Scan all devices to populate lvmcache with initial
+	 * list of PVs and VGs.
+	 */
+	lvmcache_label_scan(cmd);
+
+	/*
 	 * A list of all VGs on the system is needed when:
 	 * . processing all VGs on the system
 	 * . A VG name is specified which may refer to one
@@ -4448,7 +4450,12 @@ int process_each_pv(struct cmd_context *cmd,
 	if (!trust_cache() && !orphans_locked) {
 		log_debug("Scanning for available devices");
 		lvmcache_destroy(cmd, 1, 0);
-		dev_cache_full_scan(cmd->full_filter);
+
+		/*
+		 * Scan all devices to populate lvmcache with initial
+		 * list of PVs and VGs.
+		 */
+		lvmcache_label_scan(cmd);
 	}
 
 	if (!get_vgnameids(cmd, &all_vgnameids, only_this_vgname, 1)) {
@@ -5462,6 +5469,8 @@ int pvcreate_each_device(struct cmd_context *cmd,
 
 	dev_cache_full_scan(cmd->full_filter);
 
+	lvmcache_label_scan(cmd);
+
 	/*
 	 * Translate arg names into struct device's.
 	 */
@@ -5615,6 +5624,8 @@ int pvcreate_each_device(struct cmd_context *cmd,
 		log_error("Can't get lock for orphan PVs.");
 		goto out;
 	}
+
+	lvmcache_label_scan(cmd);
 
 	/*
 	 * The device args began on the arg_devices list, then the first check
